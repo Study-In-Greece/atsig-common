8322163d77913eab7e025724e1dfbf96022f404a
th-papaz <th.papaz@studyingreece.edu.gr>
2026-03-24 20:08:56 +0200

    Fix on policy

--- atsig_common/auth/policy.py
+++ atsig_common/auth/policy.py
@@ -1,10 +1,9 @@
 from typing import TypeVar, Generic
-
 from .context import BaseAuthContext
-
 
 ContextType = TypeVar("ContextType", bound=BaseAuthContext)
 
+
 class BaseAccessPolicy(Generic[ContextType]):
-    def __init__(self, ctx: BaseAuthContext):
-        self.ctx = ctx
+    def __init__(self, ctx: ContextType):
+        self.ctx: ContextType = ctx
